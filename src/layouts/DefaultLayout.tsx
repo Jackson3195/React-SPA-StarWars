15ea74b2fa8ddc6843c4a7c817b5228b4f69a31b
Jackson Jacob <jjpro@Jacksons-MacBook-Pro.local>
2020-09-28 18:54:55 +0100

    Base project initialised with Stylus

---
+++ src/layouts/DefaultLayout.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import DefaultPage from '../pages/DefaultPage';
+
+function DefaultLayout() {
+  return (
+    <div className="default-layout">
+      <DefaultPage></DefaultPage>
+    </div>
+  );
+}
+
+export default DefaultLayout;
---
+++ src/pages/DefaultPage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function DefaultPage() {
+  return (
+    <div className="default-page">
+      <div className="card">
+        <div className="card-header">
+          <h6>STAR WARS HEROES</h6>
+        </div>
+        <div className="card-body"></div>
+      </div>
+    </div>
+  );
+}
+
+export default DefaultPage;
